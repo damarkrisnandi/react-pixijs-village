a12069a3d48230f272d1b657e598f4e61af602c5
damar <damar@gitlab.lab>
2023-01-08 21:00:25 +0700

    multidimensions array as tiles approach

--- src/pixi/provider.js
+++ src/pixi/provider.js
@@ -1,5 +1,6 @@
 import * as PIXI from 'pixi.js';
 import VillageMap from './villagemap';
+import { createObjectWithCollider } from "./utils/createObjectWIthCollider";
 
 export default class Provider {
     url = './pixi-assets/tileset_room.png';
@@ -25,4 +26,38 @@ export default class Provider {
         y: Math.floor((window.innerHeight / 2) / this.unit) + 1 - Math.floor(this.villageMap.mapSize.y / 2)
     };
     
+    indexOf2d(arr, val) {
+        var index = [-1, -1];
+    
+        if (!Array.isArray(arr)) {
+            return index;
+        }
+    
+        arr.some(function (sub, posX) {
+            if (!Array.isArray(sub)) {
+                return false;
+            }
+    
+            var posY = sub.indexOf(val);
+    
+            if (posY !== -1) {
+                index[0] = posX;
+                index[1] = posY;
+                return true;
+            }
+    
+            return false;
+        });
+    
+        return index;
+    }
+
+    createContainer(interiors, objects, code, name) {
+        const obj = objects.find(o => o.code === code);
+        const pos = this.indexOf2d(interiors, code);
+        if (pos[0] === -1 || pos[1] === -1) {
+            return null;
+        }
+        return createObjectWithCollider(obj.jsonObject, `${name}${obj.idx}`, this.unit, {x: this.centerMap.x + pos[1], y: this.centerMap.y + pos[0]}, obj.act, obj.msg)
+    }
 }
--- src/pixi/scenes/library.js
+++ src/pixi/scenes/library.js
@@ -5,37 +5,76 @@ import bookShelf from '../json-objects/book-shelf.json'
 import Provider from "../provider";
 import { createObjectWithCollider } from "../utils/createObjectWIthCollider";
 
-export default class LibraryScene extends Provider{
+export default class LibraryScene extends Provider {
     libraryContainer = new Container();
     createMap() {
         const roomContainer = createObjectWithCollider(room, 'room', this.unit, this.centerMap)
         this.libraryContainer.addChild(roomContainer);
-        this.placeObject() 
+        this.placeObject()
     }
 
     placeObject() {
-        const translates = [
-            {x: 1, y: 1, msg: 'Koleksi Sejarah Indonesia', act: 'info'}, 
-            {x: 1, y: 4, msg: 'Koleksi Buku Sains dan Matematika', act: 'info'}, 
-            {x: 4, y: 1, msg: 'Koleksi Buku Fiksi', act: 'info'}, 
-            {x: 4, y: 4, msg: 'Koleksi Biografi', act: 'info'}, 
-            {x: 11, y: 1, msg: 'Hobbies', act: 'info'}];
-        translates.forEach((val, idx) => {
-            // let message = null;
-            // let action = null;
-            // if (idx === 1) {
-            //     message = 'Koleksi buku Sains dan Matematika';
-            //     action = 'info';
-            // }
-            const bookShelfContainer = createObjectWithCollider(bookShelf, `bookShelf${idx}`, this.unit, {x: this.centerMap.x + val.x, y: this.centerMap.y + val.y}, val.act, val.msg)    
-            this.libraryContainer.addChild(bookShelfContainer);
-        })
-    }
-
-    getActions() {
-        return [
-            {position: {x: this.centerMap.x + 2, y: this.centerMap.y + 5}, type: 'INFO', message: 'Test message'}
+        const interiors = [
+            ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+            ['#', 'A', 'A', '_', 'B', 'B', '_', '_', '_', '_', '_', 'E', 'E', '#'],
+            ['#', 'A', 'A', '_', 'B', 'B', '_', '_', '_', '_', '_', 'E', 'E', '#'],
+            ['#', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '#'],
+            ['#', 'C', 'C', '_', 'D', 'D', '_', '_', '_', '_', '_', '_', '_', '#'],
+            ['#', 'C', 'C', '_', 'D', 'D', '_', '_', '_', '_', '_', '_', '_', '#'],
+            ['#', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '#'],
+            ['#', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '#'],
+            ['#', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '#'],
+            ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
         ]
 
+        const objects = [
+            {
+                idx: 1,
+                code: 'A',
+                msg: 'Koleksi Sejarah Indonesia',
+                act: 'info',
+                jsonObject: bookShelf,
+                container: () => this.createContainer(interiors, objects, 'A', 'bookShelf')
+            },
+            {
+                idx: 2,
+                code: 'B',
+                msg: 'Koleksi Buku Sains dan Matematika',
+                act: 'info',
+                jsonObject: bookShelf,
+                container: () => this.createContainer(interiors, objects, 'B', 'bookShelf')
+            },
+            {
+                idx: 3,
+                code: 'C',
+                msg: 'Koleksi Buku Fiksi', 
+                act: 'info',
+                jsonObject: bookShelf,
+                container: () => this.createContainer(interiors, objects, 'C', 'bookShelf')
+            },
+            {
+                idx: 4, 
+                code: 'D', 
+                msg: 'Koleksi Biografi', 
+                act: 'info',
+                jsonObject: bookShelf,
+                container: () => this.createContainer(interiors, objects, 'D', 'bookShelf')
+            },
+            {
+                idx: 5, 
+                code: 'E', 
+                msg: 'Hobbies', 
+                act: 'info',
+                jsonObject: bookShelf,
+                container: () => this.createContainer(interiors, objects, 'E', 'bookShelf')
+            }
+        ];
+
+        objects.forEach((val) => {
+            const container = val.container();
+            if (container) {
+                this.libraryContainer.addChild(container);
+            }
+        })
     }
 }
