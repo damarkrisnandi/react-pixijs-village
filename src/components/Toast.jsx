25cc455fa00ea03c86fb83317b36a6cea18b16fc
damar <damar@gitlab.lab>
2023-01-08 21:05:36 +0700

    fix react attributes

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -24,7 +24,7 @@ const Toast = ({isAlert, message, setShow}) => {
                 <div id="toast-success" className={`${animationConstant} ${!showDelay ? "translate-y-16 opacity-0": "translate-y-0 opacity-100"} flex items-center p-4 mb-4 w-full text-gray-500 bg-white rounded-lg shadow-md dark:text-gray-400 dark:bg-gray-800`} role="alert">
                     <div className="inline-flex flex-shrink-0 justify-center items-center w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
                         {/* <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg> */}
-                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                         <span className="sr-only">Check icon</span>
                     </div>
                     <div className="ml-3 text-sm transform scale-75">{message}</div>
